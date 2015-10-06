a56cf5eccec4f2bb1b96aea61ba3b5472c6bcb3d
Brian Groat <Barnum7@users.noreply.github.com>
2015-10-06 16:21:31 -0400

    Got all functionality working in one monster file. Pushing scraper with (populated) CSV

--- main.js
+++ main.js
@@ -5,7 +5,27 @@ var fs = require('fs');
 
 request('http://substack.net/images/', function (error, response, body) {
 	if(!error && response.statusCode == 200) {
-		console.log(body)
+		var $ = cheerio.load(body);
+
+		var content = [];
+
+		var stream = fs.createWriteStream("content.csv");
+		stream.once('open', function(fd) {
+			$('tr').each(function(i,elem) {
+				var fileName = elem.children[2].children[0].children[0]['data'];
+				if(/[a-zA-Z0-9]+\.[a-z]{3}/.test(fileName)){
+					var href = elem.children[2].children[0].attribs['href'];
+					var permissions = elem.children[0].children[0].children[0]['data'];
+					var fileExtension = fileName.split('.')[1]
+					stream.write(permissions + "," + href + "," + fileExtension + '\n');
+				}
+			});
+
+			stream.end();
+		});
+
+	} else {
+		console.error(error);
 	}
-});
+})
 
